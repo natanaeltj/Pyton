a23aa584c14edd59effebdd6aa1987fe7c2cc47a
Natanael <natanaelteixeiradejesus2@gmail.com>
2025-03-28 01:01:42 -0300

    firt commit

---
+++ Primeiro contato.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print('Hello world') # print == função
+
+print(2+3)
+
+# Operadores e operação matematica (// = Divisão(Somente a parte inteira), (% = Módulo/Resto da divisão),
+#(** = Exponenciação ou potenciação).
+
+notaAlun = 7.3
+if notaAlun <= 7 :
+ print('Você esta de recuperação!')
+else:
+    print('Você passou cachorro aiaiai uiui!')
