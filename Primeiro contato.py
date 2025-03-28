bb102495c9fa986e8c4f396b4c6df6cc1407fc23
Natanael <natanaelteixeiradejesus2@gmail.com>
2025-03-28 01:55:14 -0300

    Rushando assuntos da facul

--- Primeiro contato.py
+++ Primeiro contato.py
@@ -21,8 +21,23 @@ print(2+3)
 # Operadores e operação matematica (// = Divisão(Somente a parte inteira), (% = Módulo/Resto da divisão),
 #(** = Exponenciação ou potenciação).
 
-notaAlun = 7.3
+notaAlun = 7.3 #Nunca inicie o nome de uma variavel com um número!!!!!!!!!!!! só pode ser criado com underline no inicio
 if notaAlun <= 7 :
  print('Você esta de recuperação!')
 else:
     print('Você passou cachorro aiaiai uiui!')
+#tipos primitivos de dados
+
+frase = ('eita disgraceira')
+print(frase[3]) #chamando o indice da string
+
+teste = ('a' + ' ' * 20 + 'b' + ' ' * 5 + 'c')
+print(teste)
+
+disciplina = 'matematica'
+s1 = f'Você tirou {notaAlun} em {disciplina} seu fudido'  #composição com marcadores de posição modernos
+print(s1)
+print(s1[0:4]) #fatiamento string
+
+nome = input('digite o seu nome?')
+print(nome)
